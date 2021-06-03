720fa676b56124a64e083456823bfbbb98db6538
yun-n <mathpurplee@gmail.com>
2021-06-03 16:42:07 +0900

    메소드&인스터스,정적멤버

---
+++ src/instance_static/Car.java
@@ -0,0 +1,38 @@
+package instance_static;
+
+public class Car {
+	/* 인스턴스 */
+	// 필드
+	String model;
+	int speed;
+	
+	// 생성자
+	Car(String model) { // public Car(String model), public 생략 가능
+		this.model = model; // 필드 model = 파라미터 model
+	}
+	
+	// 메소드
+	void setSpeed(int speed) {
+		this.speed = speed; // 필드 speed = 파라미터 speed
+	}
+	
+	void run() {
+		for(int i=10; i<=50; i+=10) {
+			this.setSpeed(i);
+			System.out.println(this.model + "가 달립니다.(시속 : " + this.speed + "km/h)");
+		}
+	}
+	
+	/* 정적 */
+	// 필드
+	static double pi = 3.14159;
+	
+	// 메소드
+	static int plus(int x, int y) {
+		return x+y;
+	}
+	
+	static int minus(int x, int y) {
+		return x-y;
+	}
+}
---
+++ src/instance_static/CarExample.java
@@ -0,0 +1,18 @@
+package instance_static;
+
+public class CarExample {
+	public static void main(String args[]) {
+		Car myCar = new Car("Æ÷¸£½¦");
+		Car yourCar = new Car("º¥Ã÷");
+		
+		myCar.run();
+		yourCar.run();
+		
+		double result1 = Car.pi;
+		int result2 = Car.plus(10, 5);
+		int result3 = Car.minus(10, 5);
+		System.out.println("result1 : " + result1);
+		System.out.println("result2 : " + result2);
+		System.out.println("result3 : " + result3);
+	}
+}
