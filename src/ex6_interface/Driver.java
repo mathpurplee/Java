de86cd55da04aa2b000344641f7ec4b3ebbc8319
yun-n <mathpurplee@gmail.com>
2021-06-08 12:07:26 +0900

    인터페이스 - instanceof 연산자

--- src/ex6_interface/Driver.java
+++ src/ex6_interface/Driver.java
@@ -2,7 +2,7 @@ package ex6_interface;
 
 public class Driver {
 	public void drive(Vehicle vehicle) {
-		if(vehicle instanceof Bus) {   // "파라미터 vehicle이 원래 Bus타입이었다면"
+		if(vehicle instanceof Bus) {   // "파라미터 vehicle(인터페이스 Vehicle타입)이 원래 Bus타입이었다면"
 			Bus bus = (Bus)vehicle;    // 파라미터 vehicle의 타입 Vehicle을 Bus타입으로 강제타입형변환
 			bus.checkFare();
 		}
