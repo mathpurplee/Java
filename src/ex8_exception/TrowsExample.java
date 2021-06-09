0463fc403d81208a29f88865070f8c4b580bc483
yun-n <mathpurplee@gmail.com>
2021-06-09 20:10:30 +0900

    throw 키워드

---
+++ src/ex8_exception/TrowsExample.java
@@ -0,0 +1,15 @@
+package ex8_exception;
+
+public class TrowsExample {
+	public static void main(String[] args) {
+		try {
+			findClass();
+		} catch(ClassNotFoundException e) {
+			System.out.println("클래스가 존재하지 않습니다.");
+		}
+	}
+	
+	public static void findClass() throws ClassNotFoundException {
+		Class clazz = Class.forName("java.lang.String2");  // java.lang.String2의 클래스는 없다고 가정
+	}
+}
